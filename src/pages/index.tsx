bd698b381fb26e80818e9b7b49365db63b4850c7
ronkzinho <hfrcr@hotmail.com>
2021-07-16 18:44:37 -0300

    added favicon and more info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,38 +23,49 @@ export default function Home() {
     };
 
     return (
-        <div className="container">
-            <div className="header">
-                <p>Hey, how are you doing?</p>
-                <p>
-                    So I made this for make my life easier but now I just want
-                    to help for those who want to run old FSG generators locally
-                </p>
-                <p>
-                    If you don{"'"}t have the macro/consumer of this api, you
-                    can download it{" "}
-                    <a href="https://github.com/ronkzinho/oldgenoptimizer/releases/latest/download/optimizer.zip">
-                        here
-                    </a>
-                </p>
-                <button onClick={() => setSG(!showGenerators)}>
-                    {showGenerators ? "Hide" : "Show"} generators
-                </button>
-            </div>
-            {showGenerators && (
-                <div className="generators">
-                    {generators.map((gen) => (
-                        <GeneratorComponent
-                            key={gen.name}
-                            generator={gen}
-                            focused={focused === gen.name}
-                            setFocused={setFocused}
-                            currentFocus={focused}
-                            closeGen={closeGen}
-                        />
-                    ))}
+        <>
+            <Head>
+                <title>Old Gen Optimizer</title>
+            </Head>
+            <div className="container">
+                <div className="header">
+                    <p>Hey, how are you doing?</p>
+                    <p>
+                        So I made this for make my life easier but now I just
+                        want to help for those who want to run old FSG
+                        generators locally.
+                    </p>
+                    <p>
+                        If you don{"'"}t have the macro/consumer of this api,
+                        you can download it{" "}
+                        <a href="https://github.com/ronkzinho/oldgenoptimizer/releases/latest/download/optimizer.zip">
+                            here
+                        </a>
+                        . Also, you should check the{" "}
+                        <a href="https://github.com/ronkzinho/oldgenoptimizer/blob/main/README.md">
+                            README file
+                        </a>{" "}
+                        for the requirements, and how to setup everything!
+                    </p>
+                    <button onClick={() => setSG(!showGenerators)}>
+                        {showGenerators ? "Hide" : "Show"} generators
+                    </button>
                 </div>
-            )}
-        </div>
+                {showGenerators && (
+                    <div className="generators">
+                        {generators.map((gen) => (
+                            <GeneratorComponent
+                                key={gen.name}
+                                generator={gen}
+                                focused={focused === gen.name}
+                                setFocused={setFocused}
+                                currentFocus={focused}
+                                closeGen={closeGen}
+                            />
+                        ))}
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
