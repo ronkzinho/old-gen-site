628db5f6a12d02b17b6a16753dcbbd2d84830ba9
ronkzinho <hfrcr@hotmail.com>
2021-11-20 18:57:54 +0000

    made the site better

--- src/components/generator.tsx
+++ src/components/generator.tsx
@@ -1,140 +1,156 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { Generator } from "../pages/api/generators";
 
 export const GeneratorComponent: React.FC<{
-    generator: Generator;
-    focused: boolean;
-    setFocused: (value: string) => any;
-    currentFocus: string;
-    closeGen: () => any;
+  generator: Generator;
+  focused: boolean;
+  setFocused: (value: string) => any;
+  currentFocus: string;
+  closeGen: () => any;
 }> = ({ generator: gen, focused, setFocused, currentFocus, closeGen }) => {
-    const focusable = currentFocus === "";
+  const focusable = currentFocus === "";
+  const ref = useRef<HTMLDivElement>();
 
-    return (
-        <div
-            className="generatorContainer"
-            style={
-                focused
-                    ? {
-                          position: "fixed",
-                          width: "750px",
-                          height: "500px",
-                          top: 0,
-                          bottom: 0,
-                          left: 0,
-                          right: 0,
-                          margin: "auto",
-                          fontSize: "32px",
-                          zIndex: 100,
-                      }
-                    : {
-                          cursor: focusable ? "pointer" : "auto",
-                          filter: currentFocus !== "" ? "blur(10px)" : "none",
-                      }
+  const handleOutsideClick = useCallback(
+    (e: MouseEvent) => {
+      if (!ref.current?.contains(e.target as Node)) closeGen();
+    },
+    [closeGen]
+  );
+
+  useEffect(() => {
+    if (!focused) return;
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [focused, handleOutsideClick]);
+
+  return (
+    <div
+      className="generatorContainer"
+      style={
+        focused
+          ? {
+              position: "fixed",
+              width: "80%",
+              height: "90%",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              margin: "auto",
+              fontSize: "32px",
+              zIndex: 100,
+              pointerEvents: "auto",
             }
-            onClick={() => currentFocus === "" && setFocused(gen.name)}
-            key={gen.name}
+          : {
+              cursor: focusable ? "pointer" : "auto",
+              filter: currentFocus !== "" ? "blur(10px)" : "none",
+            }
+      }
+      onClick={() => {
+        focusable && setFocused(gen.name);
+      }}
+      key={gen.name}
+    >
+      <div
+        className="generator"
+        style={{
+          width: "100%",
+          height: "100%",
+          backdropFilter:
+            focused || gen.verifiable === false ? "brightness(.5)" : "none",
+          overflow: focused ? "auto" : "hidden",
+          display: "grid",
+        }}
+        ref={ref as any}
+      >
+        <div
+          style={{
+            zIndex: focused ? 101 : "auto",
+            display: "inline",
+            margin: "auto",
+            maxWidth: "80%",
+          }}
         >
+          <h1 style={gen.verifiable === false ? { color: "red" } : {}}>
+            {gen.name}
+          </h1>
+          <p style={!focused ? { color: "white" } : {}} className="sha">
+            sha256sum: {gen.sha256sum}
+          </p>
+          {focused && gen.verifiable === false && (
+            <p className="notVerifiable">
+              Runs using this filter/generator won{"'"}t be able to get verified
+            </p>
+          )}
+          {focused && (
             <div
-                className="generator"
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    backdropFilter:
-                        focused || gen.verifiable === false
-                            ? "brightness(.5)"
-                            : "none",
-                    overflow: "auto",
-                    display: "grid",
-                }}
+              style={{
+                display: "flex",
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+                flexDirection: "column",
+              }}
             >
-                <div
+              <div
+                style={{
+                  maxWidth: "90%",
+                  width: "100%",
+                  overflowY: "hidden",
+                  fontSize: "32px",
+                  padding: 0,
+                  marginBottom: 0,
+                }}
+              >
+                <p>{gen.description}</p>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textAlign: "center",
+                }}
+              >
+                {gen.source && (
+                  <a
                     style={{
-                        zIndex: focused ? 101 : "auto",
-                        display: "inline",
-                        margin: "auto",
-                        maxWidth: "80%",
+                      color: "white",
+                      textDecoration: "none",
+                      padding: 0,
+                      marginBottom: "20px",
                     }}
+                    href={gen.source}
+                  >
+                    <button className="link">Source code</button>
+                  </a>
+                )}
+                <a
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                    padding: 0,
+                  }}
+                  href={gen.url}
                 >
-                    <h1
-                        style={gen.verifiable === false ? { color: "red" } : {}}
-                    >
-                        {gen.name}
-                    </h1>
-                    <p className="sha">sha256sum: {gen.sha256sum}</p>
-                    {focused && gen.verifiable === false && (
-                        <p className="notVerifiable">
-                            Runs using this filter/generator won{"'"}t be able
-                            to get verified
-                        </p>
-                    )}
-                    {focused && (
-                        <div
-                            style={{
-                                display: "flex",
-                                flex: 1,
-                                justifyContent: "center",
-                                alignItems: "center",
-                                textAlign: "center",
-                                flexDirection: "column",
-                            }}
-                        >
-                            <div
-                                style={{
-                                    maxWidth: "90%",
-                                    width: "100%",
-                                    overflowY: "hidden",
-                                    fontSize: "32px",
-                                    padding: 0,
-                                    marginBottom: 0,
-                                }}
-                            >
-                                <p>{gen.description}</p>
-                            </div>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    flex: 1,
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                    textAlign: "center",
-                                }}
-                            >
-                                <a
-                                    style={{
-                                        color: "white",
-                                        textDecoration: "none",
-                                        padding: 0,
-                                        marginBottom: "20px",
-                                    }}
-                                    href={gen.source}
-                                >
-                                    Source code
-                                </a>
-                                <a
-                                    style={{
-                                        color: "white",
-                                        textDecoration: "none",
-                                        padding: 0,
-                                    }}
-                                    href={gen.url}
-                                >
-                                    Download it
-                                </a>
-                                {focused && (
-                                    <div
-                                        onClick={closeGen}
-                                        className="closeWrapper"
-                                    >
-                                        <a className="close"></a>
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-                    )}
-                </div>
+                  <button className="link">Download it</button>
+                </a>
+                {focused && (
+                  <div onClick={closeGen} className="closeWrapper">
+                    <a className="close"></a>
+                  </div>
+                )}
+              </div>
             </div>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
--- src/pages/api/generators.ts
+++ src/pages/api/generators.ts
@@ -4,31 +4,43 @@ import gens from "../../../public/generators.json";
 interface IGenerator extends Generator {}
 
 export class Generator implements IGenerator {
-    source?: string;
-    sha256sum: string;
-    name: string;
-    url: string;
-    description?: string;
-    verifiable?: boolean;
-    constructor({ sha256sum, name, url, description, verifiable }: IGenerator) {
-        this.sha256sum = sha256sum;
-        this.name = name;
-        this.url = url;
-        this.description = description;
-        this.source = `https://replit.com/@AndyNovo/${this.name}`;
-        this.verifiable = verifiable !== null ? verifiable : true;
-    }
+  source?: string | null;
+  sha256sum: string;
+  name: string;
+  url: string;
+  description?: string;
+  verifiable?: boolean;
+  constructor({
+    sha256sum,
+    name,
+    url,
+    description,
+    verifiable,
+    source,
+  }: IGenerator) {
+    this.sha256sum = sha256sum;
+    this.name = name;
+    this.url = url;
+    this.description = description;
+    this.source =
+      source === null
+        ? null
+        : source || `https://replit.com/@AndyNovo/${this.name}`;
+    this.verifiable = verifiable !== null ? verifiable : true;
+  }
 }
 
 export const generators: Generator[] = gens.generators.map(
-    (gen) => new Generator(gen)
+  (gen) => new Generator(gen)
 );
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json(
-        generators.reduce(
-            (obj, gen) => Object.assign(obj, { [gen.name]: gen.url }),
-            {}
-        )
+  res
+    .status(200)
+    .json(
+      generators.reduce(
+        (obj, gen) => Object.assign(obj, { [gen.name]: gen.url }),
+        {}
+      )
     );
 }
